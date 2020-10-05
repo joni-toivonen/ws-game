2b13935484c3dfbb0ae8a7527087a8e8b27a742d
Joni Toivonen <toivjoni@gmail.com>
2020-10-05 18:08:26 +0300

    Refactor input handling, add coordinate and client handling to server, add coordinate broadcasting to server, add drawing of characters to client

--- client/ws.js
+++ client/ws.js
@@ -4,52 +4,54 @@ canvas.width = 640
 canvas.height = 480
 canvas.setAttribute('tabindex', '0')
 const context = canvas.getContext('2d')
+const characterSize = 32
+const keyState = {}
+let myId
+
+const handleInput = e => {
+  const previousState = {...keyState}
+  keyState[e.code] = e.type === 'keydown'
+  for (let key of Object.keys(keyState)) {
+    if (keyState[key] !== previousState[key]) {
+      ws.send(JSON.stringify(keyState))
+      break
+    }
+  }
+}
 
 const gameArea = {
   start: () => {
     document.body.insertBefore(canvas, document.body.childNodes[0]);
     ws = new WebSocket('ws://localhost:8080')
     ws.onmessage = handleMessage
-    canvas.addEventListener('keypress', handleInput)
+    canvas.addEventListener('keydown', handleInput)
+    canvas.addEventListener('keyup', handleInput)
   },
   clear: () => {
     context.clearRect(0, 0, canvas.width, canvas.height)
   }
 }
 
-const handleMessage = message => {
-  console.log(message)
-  context.fillText(`Received: ${message.data}`, 100, 115)
+const drawPlayers = coordinates => {
+  for (let c of coordinates) {
+    context.fillRect(c.x, c.y, characterSize, characterSize)
+  }
 }
 
-const handleInput = (e) => {
-  console.log(e)
-  switch (e.charCode) {
-  case 32:
-    gameArea.clear()
-    context.fillText('Sending message:', 100, 100)
-    ws.send('space')
-    break;
-  case 97:
-    ws.send('left')
-    gameArea.clear()
-    context.fillText('Sending message:', 100, 100)
-    break;
-  case 100:
-    ws.send('right')
-    gameArea.clear()
-    context.fillText('Sending message:', 100, 100)
-    break;
-  case 115:
-    ws.send('down')
-    gameArea.clear()
-    context.fillText('Sending message:', 100, 100)
-    break;
-  case 119:
-    ws.send('up')
-    gameArea.clear()
-    context.fillText('Sending message:', 100, 100)
-    break;
+const handleMessage = message => {
+  try {
+    const data = JSON.parse(message.data)
+    if (data.hasOwnProperty('coordinates')) {
+      gameArea.clear()
+      drawPlayers(data.coordinates)
+    } else if (data.hasOwnProperty('id')) {
+      myId = data.id
+      const idElement = document.createElement('p')
+      idElement.textContent = `Your id: ${myId.toString()}`
+      document.body.appendChild(idElement)
+    }
+  } catch (error) {
+    console.log(error)
   }
 }
 
--- server/server.js
+++ server/server.js
@@ -1,17 +1,82 @@
 const WebSocket = require('ws')
 const port = 8080
+const clients = []
+const coordinates = []
+const clientInput = {}
+const width = 640
+const height = 480
+const characterSize = 32
+const speed = 5
+const gravity = 2
 
 const wss = new WebSocket.Server({
   port: port
 })
 
-wss.on('connection', ws => {
-  ws.on('message', message => {
-    console.log('message: ', message)
-    ws.send(message)
-  })
+const addClient = ws => {
+  ws.send(JSON.stringify({ id: clients.length }))
+  clients.push(ws)
+  coordinates.push({ id: clients.length - 1, x: 0, y: 0 })
+}
 
-  ws.send(`hello`)
+const handleMessage = (ws, message) => {
+  try {
+    const parsedMessage = JSON.parse(message)
+    clients.forEach((client, i) => {
+      if (client === ws) {
+        clientInput[i] = parsedMessage
+      }
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+wss.on('connection', ws => {
+  addClient(ws)
+  ws.on('message', message => {
+    handleMessage(ws, message)
+  })
 })
+
+const broadcastCoordinates = () => {
+  clients.forEach(client => {
+    client.send(JSON.stringify({ coordinates: coordinates }))
+  })
+}
+
+const handleGravity = () => {
+  for (let c of coordinates) {
+    c.y += gravity
+    if (c.y > height - characterSize) c.y = height - characterSize
+  }
+}
+
+const moveCharacters = () => {
+  for (let c of coordinates) {
+    if (clientInput[c.id] && clientInput[c.id]['KeyA']) {
+      c.x -= speed
+      if (c.x < 0) c.x = 0
+    }
+    if (clientInput[c.id] && clientInput[c.id]['KeyD']) {
+      c.x += speed
+      if (c.x > width - characterSize) c.x = width - characterSize
+    }
+    if (clientInput[c.id] && clientInput[c.id]['Space']) {
+      c.y -= speed
+      if (c.y < 0) c.y = 0
+    }
+  }
+}
+
+const updateGame = () => {
+  if (clients.length > 0) {
+    handleGravity()
+    moveCharacters()
+    broadcastCoordinates()
+  }
+}
+
+setInterval(updateGame, 16)
 
 console.log(`Started websocket server on port ${port}`)
